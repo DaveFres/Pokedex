93577fcb115b1c9d0c93dd1832c64c5e56d40444
DaveFres <nanyandave@gmail.com>
2019-05-30 19:33:34 +0300

    Change to new pokemon API and begin with Pokegame component

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./Pokecard.css";
 
+//const OLD_POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png';
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+
+let padToThree = number => (number <= 999 ? `00${number}`.slice(-3) : number);
+
 class Pokecard extends React.Component {
     render() {
         const { id, name, type, exp } = this.props;
         return (
             <div className="Pokecard">
                 <h1 className="Pokecard-title">{name}</h1>
-                <img
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
-                    alt="pokemon"
-                />
+
+                <img src={`${POKE_API}${padToThree(id)}.png`} alt="pokemon" />
 
                 <div className="Pokecard-data">type : {type}</div>
                 <div className="Pokecard-data">EXP : {exp}</div>
